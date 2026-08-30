2dc69c6c9dd2ac9278a76b00842b886ef49032d4
mateuslph <mlptche@gmail.com>
2026-08-30 19:34:07 -0300

    atualiza pom e refatora PingController

--- src/main/java/com/dash_lat_net/controller/PingController.java
+++ src/main/java/com/dash_lat_net/controller/PingController.java
@@ -2,7 +2,6 @@ package com.dash_lat_net.controller;
 
 import com.dash_lat_net.dto.PingResponseDTO;
 import com.dash_lat_net.service.PingService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,8 +11,11 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:5173")
 public class PingController {
 
-    @Autowired
-    private PingService pingService;
+    private final PingService pingService;
+
+    public PingController(PingService pingService) {
+        this.pingService = pingService;
+    }
 
     @GetMapping("/logs")
     public List<PingResponseDTO> getAllLogs() {
@@ -24,7 +26,7 @@ public class PingController {
     public List<String> getHosts() {
         return pingService.getHosts();
     }
-    
+
     @GetMapping("/{host}")
     public PingResponseDTO ping(@PathVariable String host) {
         return pingService.executarPingESalvar(host);
